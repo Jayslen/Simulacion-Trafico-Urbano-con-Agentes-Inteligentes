103b0c78cd61ddf2254fe73da096018b563dc23e
Axcell Espinal <61571507+AxlRoseRD@users.noreply.github.com>
2025-12-11 20:51:48 -0400

    Co-autored-By-<axcellEspinalNuñez> create test class layout

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TrafficSimParallel;
+namespace Tests
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new SimulationConfig(
+                mapWidth: 200,
+                mapHeight: 20,
+                agentCount: 2000,
+                simulationSteps: 10,
+                threads: 8
+            );
+
+            await TrafficSimParallel.Program.Run(config);
+        }
+    }
+}
