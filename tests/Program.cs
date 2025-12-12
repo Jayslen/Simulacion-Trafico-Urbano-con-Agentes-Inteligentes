8201dafcef027f6443d24c5a24c74a1fd6686bf3
Jayslen <jayslen134@gmail.com>
2025-12-11 21:14:31 -0400

    Co-authored-by-jayslenRojasSerrano - Add sequential test

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,16 +1,24 @@
 using TrafficSimParallel;
+
 namespace Tests
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
+            await SequentialSimulation();
+        }
+
+        public static async Task SequentialSimulation()
+        {
             var config = new SimulationConfig(
-                mapWidth: 200,
+                mapWidth: 50,
                 mapHeight: 20,
-                agentCount: 2000,
+                agentCount: 1000,
                 simulationSteps: 10,
-                threads: 8
+                sequential: true,
+                threads: 1,
+                showMap: false
             );
 
             await TrafficSimParallel.Program.Run(config);
